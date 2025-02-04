1cc332316824c80cf23a45c7b5e0a073b53d382c
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2025-02-04 18:43:48 +0000

    Added code. and All Apps feature

---
+++ components/SEO/Meta.js
@@ -0,0 +1,101 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Meta() {
+    return (
+        <Head>
+            /* Primary Meta Tags */
+            <title>Fahim' Portfolio - Creative Technologist</title>
+            <meta charSet="utf-8" />
+            <meta
+                name="title"
+                content="Fahim' Portfolio - Creative Technologist"
+            />
+            <meta
+                name="description"
+                content="Mohammad Fahim's (insertfahim) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+            />
+            <meta name="author" content="Mohammad Fahim (insertfahim)" />
+            <meta
+                name="keywords"
+                content="insertfahim, insertfahim's portfolio, insertfahim linux, ubuntu portfolio, insert fahim protfolio,insertfahim computer, insert fahim, insertfahim ubuntu, Mohammad Fahim ubuntu portfolio"
+            />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
+            <meta name="theme-color" content="#E95420" />
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta
+                itemProp="name"
+                content="insert fahim Portfolio - Computer Engineering Student"
+            />
+            <meta
+                itemProp="description"
+                content="Mohammad Fahim's (insertfahim) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+            />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta
+                name="twitter:title"
+                content="Mohammad Fahim's (insertfahim) Portfolio - Computer Engineering Student"
+            />
+            <meta
+                name="twitter:description"
+                content="Mohammad Fahim's (insertfahim) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+            />
+            <meta
+                name="twitter:site"
+                content="Mohammad Fahim's (insertfahim)"
+            />
+            <meta
+                name="twitter:creator"
+                content="Mohammad Fahim's (insertfahim)"
+            />
+            <meta
+                name="twitter:image:src"
+                content="public\images\logos\fevicon.png"
+            />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta
+                name="og:title"
+                content="Mohammad Fahim's (insertfahim) Portfolio - Computer Engineering Student"
+            />
+            <meta
+                name="og:description"
+                content="Mohammad Fahim's (insertfahim) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
+            />
+            <meta name="og:image" content="public\images\logos\fevicon.png" />
+            <meta name="og:url" content="http://inserfahim.github.io/" />
+            <meta
+                name="og:site_name"
+                content="Mohammad Fahim's (insertfahim) Personal Portfolio"
+            />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+            <link
+                rel="icon"
+                href="https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://neetcode.io/&size=48"
+            />
+            <link
+                rel="apple-touch-icon"
+                href="public\images\logos\fevicon.png"
+            />
+            <link
+                rel="preload"
+                href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+                as="style"
+            />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+                rel="stylesheet"
+            ></link>
+        </Head>
+    );
+}
